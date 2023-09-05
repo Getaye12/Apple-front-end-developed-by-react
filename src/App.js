0fc87b64179449a90bbc4b1d4124cec7e005ab14
Getaye12 <asfawgetaye123@gmail.com>
2023-09-05 01:28:01 -0700

    Apple front replica end developed by React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './Components/Header/header';
+import Footer from './Components/Footer/Footer';
+import SectionSix from './Components/SectionSix/SectionSix';
+import FivthSection from './Components/FivthSection/FivthSection';
+import FourthSection from './Components/FourthSection/FourthSection';
+import ThirdSection from './Components/ThirdSection/ThirdSection';
+import SecondSection from './Components/SecondSection/SecondSection';
+import FirstSection from './Components/FirstSection/FirstSection';
+import AlertSection from './Components/AlertSection/AlertSection';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+     <AlertSection/>
+      <FirstSection/>
+      <SecondSection/>
+      <ThirdSection/>
+      <FourthSection/>
+      <FivthSection/>
+      <SectionSix/>
+      <Footer />
+     
     </div>
   );
 }
